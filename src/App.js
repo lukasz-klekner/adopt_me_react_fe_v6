ec229a8d51857fee601818dc9b193e471e193a8f
Łukasz Klekner <lukaszklekner@gmail.com>
2021-11-23 09:12:01 +0100

    adds props to Pet component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-const Pet = () =>
+const Pet = ({ animal, name }) =>
   React.createElement('div', {}, [
-    React.createElement('h2', {}, 'Dog'),
-    React.createElement('h3', {}, 'Luna'),
-    React.createElement('h3', {}, 'Reksio'),
+    React.createElement('h2', {}, animal),
+    React.createElement('h3', {}, name),
   ])
 
 const App = () =>
   React.createElement('div', {}, [
     React.createElement('h1', { id: 'header' }, 'Adopt me'),
     ...[1, 2, 3, 4].map((i) => React.createElement('div', {}, i)),
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet),
+    React.createElement(Pet, { animal: 'Dog', name: 'Luna' }),
+    React.createElement(Pet, { animal: 'Cat', name: 'Filemon' }),
+    React.createElement(Pet, { animal: 'Hamster', name: 'Kruszynka' }),
   ])
 
 ReactDOM.render(React.createElement(App), document.getElementById('root'))
