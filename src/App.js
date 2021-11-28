6fde2ed72543160a0855a80e03af9c2b8b59a268
Łukasz Klekner <lukaszklekner@gmail.com>
2021-11-28 08:30:12 +0100

    modifies App.js and uses SearchParams component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import ReactDOM from "react-dom";
 
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => (
   <div>
     <h1>Adopt me!</h1>
-    <Pet animal="Dog" name="Luna" />
-    <Pet animal="Cat" name="Filemon" />
-    <Pet animal="Hamster" name="Kruszynka" />
+    <SearchParams />
   </div>
 );
 
