c61563ad9bcea25467d4f7b10f76ba4f7173f446
Łukasz Klekner <lukaszklekner@gmail.com>
2021-11-28 08:29:32 +0100

    creates SearchParams component

---
+++ src/SearchParams.js
@@ -0,0 +1,27 @@
+import { even } from "prelude-ls";
+import { useState } from "react";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("Seattle, WA");
+
+  const updateLocation = (event) => setLocation(event.target.value);
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            onChange={updateLocation}
+            placeholder="Location"
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
