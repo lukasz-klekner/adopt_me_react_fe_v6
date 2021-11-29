350776584cf5e9e038399743c20809efbf21b399
Łukasz Klekner <lukaszklekner@gmail.com>
2021-11-29 11:54:49 +0100

    adds animal and breeds state to SearchParams

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,9 +1,15 @@
 import { useState } from "react";
 
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("");
+  const [breeds, setBreeds] = useState([]);
 
   const updateLocation = (event) => setLocation(event.target.value);
+
+  const handleSelectChange = (event) => setAnimal(event.target.value);
 
   return (
     <div className="search-params">
@@ -16,6 +22,38 @@ const SearchParams = () => {
             onChange={updateLocation}
             placeholder="Location"
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={handleSelectChange}
+            onBlur={handleSelectChange}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option value={animal} key={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breeds}
+            onChange={handleSelectChange}
+            onBlur={handleSelectChange}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option value={breed} key={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
