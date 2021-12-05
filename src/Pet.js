87a0ae9752bac0cc67a45b5f7d4caa9701fb08e1
Łukasz Klekner <lukaszklekner@gmail.com>
2021-12-05 09:52:16 +0100

    refactors Pet component

--- src/Pet.js
+++ src/Pet.js
@@ -1,9 +1,18 @@
-const Pet = ({ animal, name, breed }) => (
-  <div>
-    <h2>{animal}</h2>
-    <h3>{name}</h3>
-    <h3>{breed}</h3>
-  </div>
-);
+const Pet = ({ animal, name, breed, id, images, location }) => {
+  let hero = "http://pets-images.dev-apis.com/pets/none.jpg"
 
+  if(images.length) hero=images[0]
+
+  return(
+    <a className='pet' href={`/details/${id}`}>
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+  <div className='info'>
+    <h2>{name}</h2>
+    <h3>{`${animal} - ${breed} - ${location}`}</h3>
+  </div>
+  </a>
+);
+}
 export default Pet;
