aeba05f8299a50d878c166aff8c3af1ff5dabdf7
Łukasz Klekner <lukaszklekner@gmail.com>
2021-12-05 11:28:16 +0100

    install react router dom and sets up a basic structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import './App.css'
+import Details from './Details';
 
 import SearchParams from "./SearchParams";
 
 const App = () => (
   <div>
     <h1>Adopt me!</h1>
-    <SearchParams />
+    <Router>
+      <Switch>
+        <Route path='/details/:id'>
+          <Details />
+        </Route>
+        <Route path='/'>
+          <SearchParams />
+        </Route>
+      </Switch>
+    </Router>
+
   </div>
 );
 
---
+++ src/Details.js
@@ -0,0 +1,5 @@
+const Details = () => (
+    <h3>Details!!!</h3>
+)
+
+export default Details
