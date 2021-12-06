131e3a84aaff31597209b8de787e90555a323f56
Łukasz Klekner <lukaszklekner@gmail.com>
2021-12-06 10:34:32 +0100

    implements Error Boundry component

--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,7 @@
 import { Component } from "react"
 import {withRouter} from 'react-router-dom'
 import Carousel from "./Carousel"
+import ErrorBoundary from "./ErrorBoundary"
 class Details extends Component {
     constructor(){
         super()
@@ -23,19 +24,17 @@ class Details extends Component {
         }
 
         const { animal, breed, city, state, description, name, images } = this.state;
+        throw new Error('Siemka')
 
-        return (
-          <div className="details">
-              <Carousel images={images}/>
-            <div>
-              <h1>{name}</h1>
-              <h2>{`${animal} — ${breed} — ${city}, ${state}`}</h2>
-              <button>Adopt {name}</button>
-              <p>{description}</p>
-            </div>
-          </div>
-        );
       }
 }
 
-export default withRouter(Details)
+const DetailsWithRouter = withRouter(Details)
+
+export default function DetailsWithErrorBoundary(props) {
+    return (
+        <ErrorBoundary>
+            <DetailsWithRouter {...props} />
+        </ErrorBoundary>
+    )
+}
