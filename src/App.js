5aee8c08980c1f9ee6e5e84f030750e1300573b5
Łukasz Klekner <lukaszklekner@gmail.com>
2021-12-28 09:50:25 +0100

    implements code splitting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { useState } from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { useState, Suspense, lazy } from 'react'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 
 import './App.css'
-import Details from './Details';
+import ThemeContext from './ThemeContext'
 
-import SearchParams from "./SearchParams";
-import ThemeContext from './ThemeContext';
+const Details = lazy(() => import('./Details'))
+const SearchParams = lazy(() => import('./SearchParams'))
 
 const App = () => {
   const theme = useState('darkblue')
@@ -13,24 +13,26 @@ const App = () => {
   return (
     <ThemeContext.Provider value={theme}>
       <div>
-        <Router>
-        <header>
-          <Link to='/'>
-              <h1>Adopt me!</h1>
-          </Link>
-          </header>
-          <Switch>
-            <Route path='/details/:id'>
-              <Details />
-            </Route>
-            <Route path='/'>
-              <SearchParams />
-            </Route>
-          </Switch>
-        </Router>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Router>
+            <header>
+              <Link to='/'>
+                <h1>Adopt me!</h1>
+              </Link>
+            </header>
+            <Switch>
+              <Route path='/details/:id'>
+                <Details />
+              </Route>
+              <Route path='/'>
+                <SearchParams />
+              </Route>
+            </Switch>
+          </Router>
+        </Suspense>
       </div>
     </ThemeContext.Provider>
-  );
+  )
 }
 
 export default App
