a42f8da50b4f0e2f81fbd4ffc626004627f73ee0
Łukasz Klekner <lukaszklekner@gmail.com>
2022-01-01 08:48:57 +0100

    initializes provider with store

--- src/App.js
+++ src/App.js
@@ -1,38 +1,35 @@
-import { useState, Suspense, lazy } from 'react'
+import { Suspense, lazy } from 'react'
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
+import store from './store'
 import './App.css'
-import ThemeContext from './ThemeContext'
 
 const Details = lazy(() => import('./Details'))
 const SearchParams = lazy(() => import('./SearchParams'))
 
-const App = () => {
-  const theme = useState('darkblue')
-
-  return (
-    <ThemeContext.Provider value={theme}>
-      <div>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Router>
-            <header>
-              <Link to='/'>
-                <h1>Adopt me!</h1>
-              </Link>
-            </header>
-            <Switch>
-              <Route path='/details/:id'>
-                <Details />
-              </Route>
-              <Route path='/'>
-                <SearchParams />
-              </Route>
-            </Switch>
-          </Router>
-        </Suspense>
-      </div>
-    </ThemeContext.Provider>
-  )
-}
+const App = () => (
+  <Provider store={store}>
+    <div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Router>
+          <header>
+            <Link to='/'>
+              <h1>Adopt me!</h1>
+            </Link>
+          </header>
+          <Switch>
+            <Route path='/details/:id'>
+              <Details />
+            </Route>
+            <Route path='/'>
+              <SearchParams />
+            </Route>
+          </Switch>
+        </Router>
+      </Suspense>
+    </div>
+  </Provider>
+)
 
 export default App
