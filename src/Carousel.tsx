33932edc3989605f23215148b446c36616d26aa0
Łukasz Klekner <lukaszklekner@gmail.com>
2022-01-07 09:04:17 +0100

    refactors Carousel to TS

---
+++ src/Carousel.tsx
@@ -0,0 +1,49 @@
+import { Component, MouseEvent, ReactNode } from 'react'
+
+interface IProps {
+  images: string[]
+}
+
+class Carousel extends Component<IProps> {
+  state = {
+    active: 0,
+  }
+
+  static defaultProps = {
+    images: ['http://pets-images.dev-apis.com/pets/none.jpg'],
+  }
+
+  handleIndexClick = (event: MouseEvent<HTMLElement>): void => {
+    if (!(event.target instanceof HTMLElement)) return
+
+    if (event.target.dataset.index)
+      this.setState({
+        active: +event.target.dataset.index,
+      })
+  }
+
+  render(): ReactNode {
+    const { active } = this.state
+    const { images } = this.props
+
+    return (
+      <div className='carousel'>
+        <img src={images[active]} alt='animal' />
+        <div className='carousel-smaller'>
+          {images.map((photo, index) => (
+            <img
+              key={photo}
+              data-index={index}
+              src={photo}
+              className={index === active ? 'active' : ''}
+              alt='animal thumbnail'
+              onClick={this.handleIndexClick}
+            />
+          ))}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Carousel
